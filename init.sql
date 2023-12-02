5c01e5a52b259d538d7976e29730e01c308f4e93
ashok.v@sen-sei.in <ashok.v@sen-sei.in>
2023-12-02 08:31:17 +0530

    Updated init.sql scripts

--- init.sql
+++ init.sql
@@ -1,14 +1,7 @@
-truncate table job;
-drop table ec2_instance;
-drop table s3_bucket;
-drop table s3_objects;
-
-
 CREATE TABLE IF NOT EXISTS job (
     id VARCHAR(255) PRIMARY KEY,
     status VARCHAR(255) NOT NULL
 );
-
 
 CREATE TABLE IF NOT EXISTS ec2_instance (
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -16,7 +9,6 @@ CREATE TABLE IF NOT EXISTS ec2_instance (
     job_id VARCHAR(255),
     FOREIGN KEY (job_id) REFERENCES job (id) ON DELETE CASCADE
 );
-
 
 CREATE TABLE IF NOT EXISTS s3_bucket (
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -33,15 +25,3 @@ CREATE TABLE s3_objects (
     job_id VARCHAR(255),
     FOREIGN KEY (job_id) REFERENCES job (id)
 );
-
-
-SELECT @@sql_mode;
-select * from job;
-select * from ec2_instance ei;
-select * from s3_bucket sb;
-select * from s3_objects;
-
-
-SHOW CREATE TABLE job;
-
-delete from job; 
